88952d0e21dd27cf4c2e49383c8ea4ff45b26d4e
Juan del Valle Ruiz <juangdelvalle@Juans-MacBook-Pro.local>
2015-03-17 23:42:45 -0500

    Initial Commit

---
+++ clinicadelvalle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clinicadelvalle
+//
+//  Created by Juan del Valle Ruiz on 3/17/15.
+//  Copyright (c) 2015 Juan del Valle Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
